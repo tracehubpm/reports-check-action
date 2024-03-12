27d359a4c6c092df4a911249890693cd18398506
h1alexbel <hialexbel@gmail.com>
2024-03-12 14:02:24 +0300

    feat(#27): example attention

--- dist/index.js
+++ dist/index.js
@@ -595,7 +595,7 @@ class UserPrompt {
     If it does not look like a bug report, then just say "Not a bug report".
     If bug report does follow all the rules, please rate such bug report as "awesome" by answering just "Quality is awesome".
     Otherwise, please say what needs to be improved, this must contain only 1-2 sentence bullet points, mainly focusing on the context of the bug report, as well as the rules.
-    Each bullet point should start with a context-specific comments.
+    Each bullet point should start with a context-specific comments, please pay attention to a possible examples in the bug report itself.
     Each bullet point should link the bug report context with the violated rule.
     Print only the bullet points, without any other info.
     Please strictly adhere to the example template provided.
