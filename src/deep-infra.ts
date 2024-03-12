eca5a6183613d7b66c5fdd9b0910714157b159d4
h1alexbel <hialexbel@gmail.com>
2024-03-12 11:55:34 +0300

    feat(#27): 0.2 temp

--- src/deep-infra.ts
+++ src/deep-infra.ts
@@ -61,7 +61,7 @@ export class DeepInfra {
         method: 'POST',
         body: JSON.stringify({
           model: this.model,
-          temperature: 0.1,
+          temperature: 0.2,
           messages: [
             {
               role: "system",
