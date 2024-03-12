d315f30f0508c7c0d716c376b5e3d3b13c39ee85
h1alexbel <hialexbel@gmail.com>
2024-03-12 16:22:38 +0300

    feat(#30): too abstract stopper

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -65,6 +65,7 @@ export class UserPrompt {
     Please review the quality of following bug report and generate analysis summary.
     If it does not look like a bug report, then just say "Not a bug report".
     If bug report does follow all the rules, please rate such bug report as "awesome" by answering just "Quality is awesome".
+    If bug report is too abstract then please reject it by answering "Bug report is too abstract".
     Otherwise, please say what needs to be improved, this must contain only 1-2 sentence bullet points, mainly focusing on the context of the bug report, as well as the rules.
     Each bullet point should start with a context-specific comments, please pay attention to a possible examples in the bug report itself.
     Each bullet point should link the bug report context with the violated rule.
