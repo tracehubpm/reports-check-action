4ddd2f0d1531122b480c80fc165480d6f418e704
h1alexbel <hialexbel@gmail.com>
2024-03-14 20:56:00 +0300

    feat(#48): only bullet points

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -61,7 +61,7 @@ export class UserPrompt {
     And everything that "doesn't look right".
     If it does not have a clear problem statement, then just say "Not a bug report".
     Please tell if bug report has some quality problems according to our rules,
-    summarize them into bullet points sentences filled with context semantics and what needs to be fixed.
+    summarize them into bullet-points-only sentences filled with context semantics and what needs to be fixed.
     If bug report fits all the rules, don't generate summary, instead, rate such bug report as "awesome" by answering just "Quality is awesome".
     Even it does not look like a bug report still don't copy the example.
     Rules=[
