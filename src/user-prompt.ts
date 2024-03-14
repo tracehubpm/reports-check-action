913b857003ab342b25b08d2ee581c6ef2a143a32
h1alexbel <hialexbel@gmail.com>
2024-03-14 21:06:10 +0300

    feat(#48): negative

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -63,6 +63,7 @@ export class UserPrompt {
     If it does not have a clear problem statement, then just say "Not a bug report".
     Please tell if bug report has some quality problems according to our rules,
     summarize them into bullet-points-only sentences filled with context semantics and what needs to be fixed.
+    Tell only negative things.
     If bug report fits all the rules, don't generate summary, instead, rate such bug report as "awesome" by answering just "Quality is awesome".
     Even it does not look like a bug report still don't copy the example.
     Rules=[
