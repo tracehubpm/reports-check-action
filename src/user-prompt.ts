b1860f45a97aee3c0ce9d63778633a9100f4efc5
h1alexbel <hialexbel@gmail.com>
2024-03-15 14:22:54 +0300

    feat(#48): fix it

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -49,8 +49,8 @@ export class UserPrompt {
     return `
     Please review following bug report.
     If bug report formulation does not match the rules, then please generate summary
-    to the report author about what to fix in this report, so it will match the violated rules better.
-    Please include in your summary only bullet point sentences about rules that bug report violates, supplied with useful context information.
+    to the report author about what to fix in this report, so author can fix it using this summary.
+    Please include in your summary only bullet point sentences about rules that bug report violates, supplied with useful context-rich information.
     The information and rule match must be precise.
     Don't generate any other information.
     If bug report does only match all the rules, then please don't generate summary, instead, just say "Quality is awesome".
