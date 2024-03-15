94bf55c66401e75287841bb96a6d3887805ce9c1
h1alexbel <hialexbel@gmail.com>
2024-03-15 13:54:51 +0300

    feat(#48): violated

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -49,8 +49,8 @@ export class UserPrompt {
     return `
     Please review following bug report.
     If bug report formulation does not match the rules, then please generate summary
-    to the report author about what to fix in this report, so it will match the rules better.
-    Please include in your summary only bullet points sentences, supplied with useful context information.
+    to the report author about what to fix in this report, so it will match the violated rules better.
+    Please include in your summary only bullet points sentences about rules that bug report violates, supplied with useful context information.
     This information must be precise.
     If bug report does only match all the rules, then please don't generate summary, instead, just say "Quality is awesome".
     Rules match must be precise.
