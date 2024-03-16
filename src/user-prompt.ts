84cc3c7f0f6c38bee38c4ddb02eb66f7aa22fb9d
h1alexbel <hialexbel@gmail.com>
2024-03-16 10:39:31 +0300

    feat(#47, #48): simple

--- src/user-prompt.ts
+++ src/user-prompt.ts
@@ -44,20 +44,10 @@ export class UserPrompt {
   value(): string {
     return `
     Please review following bug report.
-    Bug report can be formulated in different ways, so it's very important to look deeply in its context to identify does report matches or not to the specific rule.
-    If bug report formulation does not match the rules, then please generate a summary to the bug report author.
-    The summary must include only violated rules and the report context information that can help to fix the report.
-    It's very important to not include matched rules.
-    Don't include any other information in your summary.
-    Please strictly adhere the example template provided.
-    If bug report does only match all the rules, then please don't generate summary, instead, just say "Quality is awesome".
-    Rules=[
-    Bug report should have problem statement, examples: any problem statement essential formulation, so it clear that "there is a problem";
-    Bug report should have what is expected to see, examples: expected behavior or expected values in which system archives desired state;
-    Bug report should have it's context, examples: provided code snippet or reference to class or file like EmailsTest.java
-    ]
-    Summary Example:
-    * <violated rule, context>
+    If bug report formulation has quality problems, then please generate a summary to the bug report
+    author. The summary must include only quality problems with the report and how to fix them, so
+    author can formulate the report better.
+    If bug report is clean, then please don't generate summary, instead, just say "Quality is awesome".
     Bug report:
     ${this.report}
     `;
