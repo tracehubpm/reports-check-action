6dbce782cbc86f2bb330f5de9ce6d9e0350390f9
h1alexbel <hialexbel@gmail.com>
2024-03-18 15:08:50 +0300

    data print

--- src/blob.ts
+++ src/blob.ts
@@ -12,11 +12,12 @@ export class Blob {
       ref: 'master', // read default branch
       path: 'src/main/java/git/tracehub/tk/TkGitHub.java' // file path after deterministic parsing
     });
-    const data = response.data.toString();
-    if (data) {
-      console.log(JSON.parse(data))
+    console.log(response.data);
+    // const data = response.data.toString();
+    // if (data) {
+    //   console.log(JSON.parse(data))
       // const decoded = Base64.decode(JSON.parse(data));
       // console.log(decoded);
-    }
+    // }
   }
 }
