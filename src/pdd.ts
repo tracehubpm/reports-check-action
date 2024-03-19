18cfdca82b12701e27743656421e9f31705b6d42
h1alexbel <hialexbel@gmail.com>
2024-03-19 09:30:18 +0300

    no await

--- src/pdd.ts
+++ src/pdd.ts
@@ -41,11 +41,11 @@ export class Pdd {
    * Run it.
    */
   async run() {
-    const puzzle = new Ranged(
-      await new Blob(this.github).value(),
+    const puzzle = await new Ranged(
+      new Blob(this.github),
       "150-156" // from parsed link
       // /https://github.com/tracehubpm/tracehub/blob/8d2aca048e33a5c9d83a49af4246c9ad7fde9998/src/main/java/git/tracehub/tk/TkGitHub.java#L150-L156
-    ).asText();
+    ).value();
     console.log(puzzle);
   }
 }
