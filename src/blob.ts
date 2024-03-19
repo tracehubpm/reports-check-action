bd68130e8cd2a52f024357b4e4f73168df16ac33
h1alexbel <hialexbel@gmail.com>
2024-03-19 09:09:04 +0300

    default branch

--- src/blob.ts
+++ src/blob.ts
@@ -1,16 +1,56 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2023-2024 Tracehub.git
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 import {Octokit} from "@octokit/rest";
 import {Base64} from "js-base64";
 
+/**
+ * Code tree GitHub blob.
+ */
 export class Blob {
 
+  /**
+   * Ctor.
+   * @param github GitHub
+   */
   constructor(private readonly github: Octokit) {
   }
 
+  /**
+   * As text.
+   */
   async asText(): Promise<string[]> {
+    const {data} = await this.github.repos.get(
+      {
+        owner: 'tracehubpm',
+        repo: 'tracehub'
+      }
+    );
+
     const response = await this.github.repos.getContent({
       owner: 'tracehubpm', // issue.owner
       repo: 'tracehub', // issue.repo
-      ref: 'master', // read default branch
+      ref: data.default_branch,
       path: 'src/main/java/git/tracehub/tk/TkGitHub.java' // file path after deterministic parsing
     });
     const encoded = JSON.parse(JSON.stringify(response.data)).content;
